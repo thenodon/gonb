c108d7009ddad896813c8b973d383985c1960c82
andersh <anders.haal@ingby.com>
2023-02-20 12:58:26 +0100

    Fix bug in module import

--- json_team_gonb_provider/__main__.py
+++ json_team_gonb_provider/__main__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from json_team_dto_gonb_provider.json_file import execute
+from json_team_gonb_provider.json_file import execute
 if __name__ == "__main__":
     execute()
     
